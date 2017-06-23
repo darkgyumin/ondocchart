41a06a5ebcbd71d43649341956fc984ed95cd283
KimGyuMin <blackksj@naver.com>
2017-06-23 13:42:02 +0900

    Sheet
    수정중

--- app/classes/Sheet.js
+++ app/classes/Sheet.js
@@ -9,13 +9,29 @@ export default class Sheet {
 
         splSheet.forEach(function(data) {
             let splSheet = data.split('|^@3@^|');
+
+            if(splSheet[0] == 'PAGE_NAMEVALUE') {
+                rtnObj[splSheet[0]] = Sheet.property(splSheet[1]);                
+            }
             
-            console.log(splSheet);
-            rtnObj[splSheet[0]] = splSheet[1];
+            //if(rtnObj[splSheet[0]] == undefined) rtnObj[splSheet[0]] = [];
+            //rtnObj[splSheet[0]] = splSheet[1];
         });
 
         console.log(rtnObj);
 
         return sheet;
+    }
+
+    //parameter value
+    //return object
+    //세부서식을 객체화
+    static property(value) {
+        let rtnObj = {};
+
+        let splValue = value.split('|^@2@^|');
+        console.log(splValue);
+
+        return {};
     }
 }
