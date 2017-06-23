0b76a4aafcd014439d46f1007afb9bc30c328f84
KimGyuMin <blackksj@naver.com>
2017-06-23 10:06:28 +0900

    init
    init

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+var path = require('path');
+var webpack = require('webpack');
+var WebpackBrowserPlugin = require('webpack-browser-plugin');
+
+module.exports = {
+    entry: ['./app/index.js'],
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /(node_modules)/,
+            use: [{
+                loader: 'babel-loader',
+                options: {
+                    presets: [['es2015', {modules: false}]]
+                }
+            }]
+        },
+            {
+                test: /\.css$/,
+                use: [ 'style-loader', 'css-loader' ]
+            }]
+    },
+    devServer: {
+        hot: true,
+        port: 3000, //개발서버 포트 지정
+        contentBase: path.resolve(__dirname, 'dist'), //기본 context
+        publicPath: '/'
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new WebpackBrowserPlugin({
+            browser: 'Explorer',
+            port: '3000',
+            url: 'http://127.0.0.1'
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false
+            }
+        })
+    ]
+};
