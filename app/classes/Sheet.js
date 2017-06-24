8ba5923f589938fcf82485ae89c43c5f30b5e27b
KimGyuMin <blackksj@nate.com>
2017-06-24 22:39:43 +0900

    Sheet Load
    수정중

--- app/classes/Sheet.js
+++ app/classes/Sheet.js
@@ -1,26 +1,45 @@
 export default class Sheet {
     constructor() {}
-
+    
+    //서식파일을 객체화
     static load(sheet) {
         //sheet 서식 데이터
         let rtnObj = {};
+        let tempPAGE_NAMEVALUE = null;
+        let tempPAGE_NAMEVALUE_index = 0;
+        let tempPANEL_NAMEVALUE = null;
+        let tempPANEL_NAMEVALUE_index = 0;
         
         let splSheet = sheet.split('|^@4@^|');
 
-        splSheet.forEach(function(data) {
+        splSheet.forEach((data) => {
             let splSheet = data.split('|^@3@^|');
 
             if(splSheet[0] == 'PAGE_NAMEVALUE') {
-                rtnObj[splSheet[0]] = Sheet.property(splSheet[1]);                
+                rtnObj[splSheet[0]] = Sheet.property(splSheet[1]);
+
+                tempPAGE_NAMEVALUE = rtnObj[splSheet[0]];
+            } else if(splSheet[0] == 'PANEL_NAMEVALUE') {
+
+                if(tempPAGE_NAMEVALUE[splSheet[0]] === undefined) tempPAGE_NAMEVALUE[splSheet[0]] = [];
+
+                tempPAGE_NAMEVALUE[splSheet[0]][tempPAGE_NAMEVALUE_index] = Sheet.property(splSheet[1]);
+                tempPANEL_NAMEVALUE = tempPAGE_NAMEVALUE[splSheet[0]][tempPAGE_NAMEVALUE_index];
+
+                tempPAGE_NAMEVALUE_index++;
+            } else if(splSheet[0] == 'ITEM_NAMEVALUE') {
+
+                if(tempPANEL_NAMEVALUE[splSheet[0]] === undefined) tempPANEL_NAMEVALUE[splSheet[0]] = [];
+
+                tempPANEL_NAMEVALUE[splSheet[0]][tempPANEL_NAMEVALUE_index] = Sheet.property(splSheet[1]);
+
+                tempPANEL_NAMEVALUE_index++;
             }
-            
-            //if(rtnObj[splSheet[0]] == undefined) rtnObj[splSheet[0]] = [];
-            //rtnObj[splSheet[0]] = splSheet[1];
         });
 
         console.log(rtnObj);
-
-        return sheet;
+        
+        return rtnObj;
     }
 
     //parameter value
@@ -30,8 +49,12 @@ export default class Sheet {
         let rtnObj = {};
 
         let splValue = value.split('|^@2@^|');
-        console.log(splValue);
+        splValue.forEach((data) => {
+            let splValue = data.split('|^@1@^|');
 
-        return {};
+            rtnObj[splValue[0]] = splValue[1];
+        });
+
+        return rtnObj;
     }
 }
